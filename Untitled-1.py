1366ab51b4d76e5016ca9ffa7eaeb2d4a965404c
BlaBlouBlaBli <hugomiroude@gmail.com>
2025-10-30 14:20:22 +0100

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,41 @@
+
+
+"""
+#exercice 1
+
+1) correct donne "4a"
+2) faux
+3) correct donne "2-a"
+4) faux
+5) correct donne "aaaa"
+6) faux
+7) correct donne "abbc"
+8) faux
+
+#exercice 2
+
+1) correcte dit "B"
+2) dis J
+3) 7
+4) pas corrrecte
+5) Br
+6) Brrrr
+7) True
+8) False
+9) True
+10) pas correcte
+11) Bonjoura
+12) BonjourBoujour
+
+#exercice 3
+
+1) '21b'
+2) 'aab'
+3) 'ab'
+4) 'bba'
+"""
+#exercice 4
+def calcul(n):
+    a = 'c'+ n
+    return a
+print(calcul('9'))
